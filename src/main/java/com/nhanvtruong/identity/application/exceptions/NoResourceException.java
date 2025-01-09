0f315e944aab735f6096c8f5ce80e73403276514
Nhan Van Truong <truongv.nhan2812@gmail.com>
2025-01-09 16:23:53 +0700

    handle no source found exception

---
+++ src/main/java/com/nhanvtruong/identity/application/exceptions/NoResourceException.java
@@ -0,0 +1,7 @@
+package com.nhanvtruong.identity.application.exceptions;
+
+public class NoResourceException extends RuntimeException {
+  public NoResourceException(String message) {
+    super(message);
+  }
+}
